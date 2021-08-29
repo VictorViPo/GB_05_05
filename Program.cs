7e8b0e0c769c1c8ac8cd8b672f2f3a3a81182788
VictorViPo <87740269+VictorViPo@users.noreply.github.com>
2021-08-30 00:16:36 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace ConsoleApp_C1b3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+
+            Console.WriteLine("Input a:");
+
+            a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Input b:");
+
+            b = int.Parse(Console.ReadLine());
+            
+            while (a != b)
+            { 
+                if (a > b)
+                {
+                    a = a - b;
+                }
+                else
+                {
+                    b = b - a;
+                }
+            }
+            Console.WriteLine($"GDC: {a}");
+            Console.ReadKey();
+        }
+    }
+}
