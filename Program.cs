22ec6c7c11d50b5527b2c873dc12a4c50fd02d60
VictorViPo <87740269+VictorViPo@users.noreply.github.com>
2021-09-09 02:10:13 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,33 +1,12 @@
 ﻿using System;
-namespace ConsoleApp_C1b3
+
+namespace ConsoleApp_GB0505
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int a;
-            int b;
-
-            Console.WriteLine("Input a:");
-
-            a = int.Parse(Console.ReadLine());
-
-            Console.WriteLine("Input b:");
-
-            b = int.Parse(Console.ReadLine());
-            
-            while (a != b)
-            { 
-                if (a > b)
-                {
-                    a = a - b;
-                }
-                else
-                {
-                    b = b - a;
-                }
-            }
-            Console.WriteLine($"GDC: {a}");
+            Console.WriteLine("Hello World!");
             Console.ReadKey();
         }
     }
